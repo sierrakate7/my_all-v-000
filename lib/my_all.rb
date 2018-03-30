e5236a0d67fc9be77654332931e64ad5a1d9140d
Sierra McCahon <smitche4@mail.umw.edu>
2018-03-30 02:44:02 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,9 +1,10 @@
 require 'pry'
 
 def my_all?(collection)
-   i = 0
+  i = 0
+  block_return_values = []
   while i < collection.length
-    yield(collection[i])
-    i = i + 1 
+    block_return_values << yield(collection[i])
+    i = i + 1
   end
 end
